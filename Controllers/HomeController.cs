1082d47a12a7930900462afee7ec388875dfa310
Guest User <Guest@epicodus-27.local>
2018-02-06 16:30:31 -0800

    code for basic job board

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Mvc;
+using JobBoard.Models;
+using System.Collections.Generic;
+
+namespace JobBoard.Controllers
+{
+  public class HomeController : Controller
+  {
+    [HttpGet("/jobboard")]
+    public ActionResult JobBoard()
+    {
+      return View();
+    }
+    [HttpGet("/addjob")]
+    public ActionResult AddJob()
+    {
+      return View();
+    }
+    [HttpPost("/jobboard")]
+    public ActionResult CreateAddJob()
+    {
+      JobBoardVariable myJobBoard = new JobBoardVariable(Request.Form["jobName"], Request.Form["jobDescription"]);
+      myJobBoard.Save();
+      List<JobBoardVariable> allJobs = JobBoardVariable.GetAll();
+      return View("JobBoard", allJobs);
+
+    }
+  }
+}
