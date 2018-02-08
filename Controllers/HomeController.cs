05e5f07189c65061741ff82291fcd6ce4c7cfd8f
Guest User <Guest@epicodus-118.local>
2018-02-08 10:27:29 -0800

    makes rectangle project

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,40 +1,15 @@
+using System;
 using Microsoft.AspNetCore.Mvc;
-using JobBoard.Models;
-using System.Collections.Generic;
+using RectangleChecker.Models;
 
-namespace JobBoard.Controllers
+namespace RectangleChecker.Controllers
 {
   public class HomeController : Controller
   {
-    [HttpGet("/jobboard")]
-    public ActionResult JobBoard()
-    {
-      if (JobBoardVariable.GetAll() != null)
-      {
-        List<JobBoardVariable> allJobs = JobBoardVariable.GetAll();
-        return View("JobBoard", allJobs);
-      }
-      return View();
-    }
-    [HttpGet("/addjob")]
-    public ActionResult AddJob()
+    [HttpGet("/")]
+    public ActionResult Index()
     {
       return View();
     }
-    [HttpPost("/jobboard")]
-    public ActionResult CreateAddJob()
-    {
-      JobBoardVariable myJobBoard = new JobBoardVariable(Request.Form["jobName"], Request.Form["jobDescription"]);
-      myJobBoard.Save();
-      List<JobBoardVariable> allJobs = JobBoardVariable.GetAll();
-      return View("JobBoard", allJobs);
-
-    }
-    [HttpPost("/jobBoard/delete")]
-          public ActionResult DeleteAll()
-          {
-              JobBoardVariable.ClearAll();
-              return View();
-          }
   }
 }
